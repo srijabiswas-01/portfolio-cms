ef4294aeb7e335444c1bb18f1874ae41246a5ffa
SRIJA BISWAS <srijabiswas1729@gmail.com>
2026-01-03 11:30:09 +0530

    Fix static files

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -167,14 +167,15 @@ USE_TZ = True
 # --------------------------------------------------
 # Static & Media files
 # --------------------------------------------------
-# STATIC_URL = "/static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 MEDIA_URL = "/media/"
